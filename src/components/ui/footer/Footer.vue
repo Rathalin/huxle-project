fa57fe3fab5ab735f9a1b5615189c0aea65da157
Rhatalin <danielflo1997@hotmail.com>
2022-11-21 17:06:08 +0100

    Reveal duck on mobile

--- src/components/ui/footer/Footer.vue
+++ src/components/ui/footer/Footer.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="hidden sm:flex flex-col px-4 pb-1">
+  <footer class="flex flex-col px-4 pb-1">
     <hr class="w-full border-graphite-600 border-solid pb-1" />
     <div class="flex">
       <div class="invisible w-3/12"></div>
       <div
-        class="w-6/12 flex flex-col md:flex-row flex-wrap md:justify-center gap-x-4"
+        class="w-6/12 hidden sm:flex flex-col md:flex-row flex-wrap md:justify-center gap-x-4"
       >
         <span>{{ $t('footer.label') }}</span>
         <span class="hidden md:inline">|</span>
