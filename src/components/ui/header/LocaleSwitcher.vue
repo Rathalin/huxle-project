8cd3bb2fb93d0ffa38c521a6c4db30279f227444
Rathalin <danielflo1997@hotmail.com>
2022-12-01 17:29:26 +0100

    Fix build error

--- src/components/ui/header/LocaleSwitcher.vue
+++ src/components/ui/header/LocaleSwitcher.vue
@@ -1,20 +1,8 @@
 <script setup lang="ts">
-import { computed } from '@vue/reactivity'
 import { useLocaleStore } from '@/stores/locale.store'
-import type { LocaleOption } from '@/locales/i18n'
-import germany from '@/assets/images/countries/germany.png'
-import unitedKingdom from '@/assets/images/countries/united-kingdom.png'
-import grogu from '@/assets/images/grogu-48.png'
 import SubtleButton from '../buttons/SubtleButton.vue'
 
-const { $state, toggleLocale } = useLocaleStore()
-const images = new Map<LocaleOption, string>()
-images.set('en', germany)
-images.set('de', unitedKingdom)
-images.set('grogu', grogu)
-
-const selectedLocaleUpper = computed(() => $state.selectedLocale.toUpperCase())
-const selectedLocaleImage = computed(() => images.get($state.selectedLocale))
+const { toggleLocale } = useLocaleStore()
 
 function onSwitchClick() {
   toggleLocale()
