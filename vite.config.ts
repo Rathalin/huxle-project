2d70419822cc52b1daed005eda628b886f8eb66f
Andrea Haider <andihai@gmx.at>
2023-01-04 18:31:13 +0100

    set up for testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    environment: 'jsdom',
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: vitestConfig.test,
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
