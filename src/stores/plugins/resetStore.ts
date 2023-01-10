b620f2c5c87ce3328dc986800554ecbb9007d83c
Rhatalin <danielflo1997@hotmail.com>
2023-01-10 16:24:18 +0100

    Fix pinia plugin type error

--- src/stores/plugins/resetStore.ts
+++ src/stores/plugins/resetStore.ts
@@ -1,6 +1,7 @@
-import cloneDeep from 'lodash.clonedeep'
+import { cloneDeep } from 'lodash'
+import type { PiniaPluginContext } from 'pinia'
 
-export default function storeReset({ store }) {
+export default function storeReset({ store }: PiniaPluginContext) {
   const initialState = cloneDeep(store.$state)
   store.$reset = () => store.$patch(cloneDeep(initialState))
 }
